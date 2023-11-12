54ba8503188fe27b9a8e4f6d539ea5d79949082b
Your Name <you@example.com>
2023-11-13 03:02:52 +0800

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pictriage',
-    version='1.0.4',
+    version='1.0.5',
     author='Author Name',
     author_email='author@example.com',
     description='Description of my package',
